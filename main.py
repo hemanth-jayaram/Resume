dd849cf2323fb2d9ed4cceea9e9951cc0dbdb14d
Hemanth Jayaram <hemanthjayaram5566@gmail.com>
2025-04-07 17:58:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+import subprocess
+import importlib.util
+
+# Check if model is already installed
+if importlib.util.find_spec("en_core_web_sm") is None:
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+
+nlp = spacy.load("en_core_web_sm")
